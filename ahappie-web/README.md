18f43c726ef3364b5f81495775ff648dd2287dde
Daan Kentrop <43522330+D22NK@users.noreply.github.com>
2022-06-07 17:36:10 +0200

    initial commit :sparkles:

---
+++ ahappie-web/README.md
@@ -0,0 +1,3 @@
+# AHappie-web
+
+Een website voor het delen van de maaltijden die jij in jouw pauze haalt van de albert heijn maaltijdvergoeding.
---
+++ ahappie-web/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <p className="text-sky-500 font-bold">Hallo</p>;
+};
+
+export default Home;
